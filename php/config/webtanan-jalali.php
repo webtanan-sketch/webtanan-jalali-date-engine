13dd8f70d5d8757646b52472d9f5e9d0bfff4a5a
Ebrahim khasal <webtanan@gmail.com>
2026-09-02 20:27:35 +0330

    feat(v0.6.0): افزودن config قابل انتشار Laravel

---
+++ php/config/webtanan-jalali.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'calendar' => 'jalali',
+    'rtl' => true,
+    'persian_digits' => true,
+    'time' => false,
+    'seconds' => false,
+    'minute_step' => 15,
+    'holidays' => true,
+    'events' => true,
+    'storage_key' => 'webtanan-jalali-date-engine',
+];
