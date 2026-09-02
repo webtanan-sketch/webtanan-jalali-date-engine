fed7e81f37c61978c4a1b64cb22bb8a13f8799cf
Ebrahim khasal <webtanan@gmail.com>
2026-09-02 18:46:19 +0330

    v0.1.0 create public package entry

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { JalaliCore } from './core/jalali-core';
+export type { JalaliDateValue } from './core/jalali-core';
+
+export const GFTDatePicker = {
+  open() {},
+  close() {},
+  setDate() {},
+  getDate() { return null; },
+  setRange() {},
+  addEvent() {},
+  clear() {}
+};
