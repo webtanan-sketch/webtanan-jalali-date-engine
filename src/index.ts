951792d91cd81a34944e8481fce51b3333717a7e
Ebrahim khasal <webtanan@gmail.com>
2026-09-02 19:32:51 +0330

    feat(v0.5.0): تکمیل API عمومی کتابخانه

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 export { JalaliCore } from './core/jalali-core';
 export type { JalaliDateValue } from './core/jalali-core';
 
-export const GFTDatePicker = {
-  open() {},
-  close() {},
-  setDate() {},
-  getDate() { return null; },
-  setRange() {},
-  addEvent() {},
-  clear() {}
-};
+export { JalaliCalendar } from './calendar/JalaliCalendar';
+export { CalendarRenderer } from './calendar/CalendarRenderer';
+export { InteractiveCalendar } from './calendar/InteractiveCalendar';
+export { RangeSelector } from './calendar/RangeSelector';
+export { MultiDateSelector } from './calendar/MultiDateSelector';
+
+export { EventEngine } from './enterprise/EventEngine';
+export { HolidayEngine } from './enterprise/HolidayEngine';
+export { DayStatusEngine } from './enterprise/DayStatusEngine';
+export { WorkflowTimeline } from './enterprise/WorkflowTimeline';
+
+export { CRMAdapter } from './integrations/CRMAdapter';
+export { CalendarEventBridge } from './integrations/CalendarEventBridge';
+export { SalesWorkflowAdapter } from './integrations/SalesWorkflowAdapter';
+export { ProductionWorkflowAdapter } from './integrations/ProductionWorkflowAdapter';
+
+export { PersianDigits } from './utils/PersianDigits';
+export { DateValidator } from './utils/DateValidator';
+
+export { GFTDatePicker as WebtananDatePicker } from './ui/GFTDatePicker';
