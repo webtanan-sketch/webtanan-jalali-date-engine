fa92819fc64b63a9dcca14f8db05fb34b7e38b66
Ebrahim khasal <webtanan@gmail.com>
2026-09-03 16:06:17 +0330

    feat(v0.17.0): انتشار typeهای Selector و Workflow

--- src/index.ts
+++ src/index.ts
@@ -9,46 +9,29 @@ export type { CalendarNavigationKey } from './accessibility/KeyboardDateNavigato
 
 export { JalaliCalendar } from './calendar/JalaliCalendar';
 export { CalendarRenderer } from './calendar/CalendarRenderer';
+export type { CalendarCell, CalendarView } from './calendar/CalendarRenderer';
 export { InteractiveCalendar } from './calendar/InteractiveCalendar';
+export type { InteractiveCalendarOptions, InteractiveSelection } from './calendar/InteractiveCalendar';
 export { RangeSelector } from './calendar/RangeSelector';
+export type { JalaliRange, CompleteJalaliRange, RangeSelectorOptions } from './calendar/RangeSelector';
 export { MultiDateSelector } from './calendar/MultiDateSelector';
+export type { MultiDateSelectorOptions } from './calendar/MultiDateSelector';
 
 export { TimeSelector } from './time/TimeSelector';
 export type { TimeValue, TimeSelectorOptions } from './time/TimeSelector';
 
 export { EventEngine } from './enterprise/EventEngine';
-export type {
-  EventType,
-  CalendarEvent,
-  CalendarEventRecord,
-} from './enterprise/EventEngine';
+export type { EventType, CalendarEvent, CalendarEventRecord } from './enterprise/EventEngine';
 export { HolidayEngine } from './enterprise/HolidayEngine';
-export type {
-  Holiday,
-  HolidayProvider,
-  HolidaySource,
-  HolidayType,
-} from './enterprise/HolidayEngine';
+export type { Holiday, HolidayProvider, HolidaySource, HolidayType } from './enterprise/HolidayEngine';
 export { BusinessDayCalculator } from './enterprise/BusinessDayCalculator';
-export type {
-  GregorianWeekday,
-  BusinessDayCalculatorOptions,
-} from './enterprise/BusinessDayCalculator';
+export type { GregorianWeekday, BusinessDayCalculatorOptions } from './enterprise/BusinessDayCalculator';
 export { IranHolidayProvider, IRAN_FIXED_SOLAR_HOLIDAYS } from './enterprise/holidays/IranHolidayProvider';
-export type {
-  IranHolidayProviderOptions,
-  MovableIranHoliday,
-} from './enterprise/holidays/IranHolidayProvider';
+export type { IranHolidayProviderOptions, MovableIranHoliday } from './enterprise/holidays/IranHolidayProvider';
 export { AnnualHolidayDataset } from './enterprise/holidays/AnnualHolidayDataset';
-export type {
-  AnnualHolidayDatasetPayload,
-  HolidayDatasetSource,
-} from './enterprise/holidays/AnnualHolidayDataset';
+export type { AnnualHolidayDatasetPayload, HolidayDatasetSource } from './enterprise/holidays/AnnualHolidayDataset';
 export { HolidayDatasetLoader } from './enterprise/holidays/HolidayDatasetLoader';
-export type {
-  HolidayDatasetFetch,
-  HolidayDatasetFetchResponse,
-} from './enterprise/holidays/HolidayDatasetLoader';
+export type { HolidayDatasetFetch, HolidayDatasetFetchResponse } from './enterprise/holidays/HolidayDatasetLoader';
 export {
   IRAN_OFFICIAL_HOLIDAYS_1404,
   IRAN_OFFICIAL_HOLIDAYS_1405,
@@ -56,12 +39,9 @@ export {
   getIranOfficialHolidayDataset,
 } from './enterprise/holidays/IranOfficialHolidayDatasets';
 export { DayStatusEngine } from './enterprise/DayStatusEngine';
-export type {
-  DayStatus,
-  DayStatusRecord,
-  DayStatusInput,
-} from './enterprise/DayStatusEngine';
+export type { DayStatus, DayStatusRecord, DayStatusInput } from './enterprise/DayStatusEngine';
 export { WorkflowTimeline } from './enterprise/WorkflowTimeline';
+export type { WorkflowStepStatus, WorkflowStep, WorkflowStepRecord } from './enterprise/WorkflowTimeline';
 
 export { CRMAdapter } from './integrations/CRMAdapter';
 export type { CRMCustomerEvent, CRMCustomerRecord } from './integrations/CRMAdapter';
@@ -75,38 +55,21 @@ export type {
   AccountingCalendarRecord,
 } from './integrations/AccountingCalendarAdapter';
 export { SalesWorkflowAdapter } from './integrations/SalesWorkflowAdapter';
-export type {
-  SalesStage,
-  SalesStepStatus,
-  SalesStep,
-  SalesStepRecord,
-} from './integrations/SalesWorkflowAdapter';
+export type { SalesStage, SalesStepStatus, SalesStep, SalesStepRecord } from './integrations/SalesWorkflowAdapter';
 export { ProductionWorkflowAdapter } from './integrations/ProductionWorkflowAdapter';
-export type {
-  ProductionStageStatus,
-  ProductionStage,
-  ProductionOrder,
-} from './integrations/ProductionWorkflowAdapter';
+export type { ProductionStageStatus, ProductionStage, ProductionOrder } from './integrations/ProductionWorkflowAdapter';
 
 export { MemoryStorageAdapter, BrowserStorageAdapter } from './storage/StorageAdapter';
 export type { StorageAdapter, BrowserStorageLike } from './storage/StorageAdapter';
 export { DatabaseStorageAdapter } from './storage/DatabaseStorageAdapter';
 export type { DatabaseStorageDriver } from './storage/DatabaseStorageAdapter';
 export { RestStorageAdapter } from './storage/RestStorageAdapter';
-export type {
-  FetchLike,
-  FetchResponseLike,
-  RestStorageAdapterOptions,
-} from './storage/RestStorageAdapter';
+export type { FetchLike, FetchResponseLike, RestStorageAdapterOptions } from './storage/RestStorageAdapter';
 export { JsonRepository } from './storage/JsonRepository';
 export type { StoredEnvelope, JsonRepositoryOptions } from './storage/JsonRepository';
 export { DatePickerPersistence } from './storage/DatePickerPersistence';
 
-export {
-  ThemeManager,
-  WEBTANAN_THEME_PRESETS,
-  WEBTANAN_THEME_VARIABLES,
-} from './theme/ThemeManager';
+export { ThemeManager, WEBTANAN_THEME_PRESETS, WEBTANAN_THEME_VARIABLES } from './theme/ThemeManager';
 export type {
   WebtananThemeName,
   WebtananThemePreset,
@@ -125,7 +88,4 @@ export type {
   WebtananDatePickerState,
 } from './ui/WebtananDatePicker';
 
-export {
-  WebtananJalaliDatePickerElement,
-  defineWebtananJalaliDatePicker,
-} from './web-component/WebtananJalaliDatePickerElement';
+export { WebtananJalaliDatePickerElement, defineWebtananJalaliDatePicker } from './web-component/WebtananJalaliDatePickerElement';
